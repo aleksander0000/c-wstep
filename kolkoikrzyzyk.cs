7bab7cc34e0b38fc2a2340202f8e8920c1c49540
aleksander0000 <75944453+aleksander0000@users.noreply.github.com>
2022-02-10 17:53:10 +0100

    Add files via upload

---
+++ kolkoikrzyzyk.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public bool czyjatura = false;
+        //false kolko x krzyzyk
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+            
+            private void button1_Click(object sender, EventArgs e)
+
+        {
+            
+            string cowyswietlic;
+            string przeciwna;
+            if (czyjatura == false)
+                
+            {
+                cowyswietlic = "X";
+                przeciwna = "O";
+                czyjatura = true;
+
+            }
+            else
+            {
+                cowyswietlic = "O";
+                przeciwna = "X";
+                czyjatura = false;
+            }
+            button1.Text = cowyswietlic;
+            textBox1.Text = przeciwna;
+            textBox1.Text = "test22";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
